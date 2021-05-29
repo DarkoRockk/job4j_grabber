d415cf5405779e4a6324bcf2cb17255927c8a290
darkorockk <shadrin.dev@gmail.com>
2021-05-29 17:54:29 +0300

    4. Схема таблицы Post [#1731 #161359]

--- create.sql
+++ create.sql
@@ -2,8 +2,8 @@ create database if not exists grabber;
 
 create table if not exists post(
     id serial primary key,
-    name unique varchar(255),
-    text varchar(2500),
-    link varchar(255),
-    created date
+    name unique text,
+    text text,
+    link text,
+    created timestamp
 );
